3489e8c2f5b12151de931b402df68d8cf8eb25fd
Scott Fleischman <scott.fleischman@logos.com>
2016-02-04 16:49:45 -0800

    Add andAll and use Seq.exists.

--- AdventOfCode/Day05.fs
+++ AdventOfCode/Day05.fs
@@ -2,27 +2,30 @@
 
 let vowels = "aeiou" |> Set.ofSeq
 let isVowel x = Set.contains x vowels
-let getTrueCount = Seq.sumBy (function | true -> 1 | false -> 0)
-let vowelCount (text : string) = text |> Seq.map isVowel |> getTrueCount
 
 let hasAtLeast n p =
     Seq.scan (fun s x -> s + if p x then 1 else 0) 0
-    >> Seq.tryPick (fun x -> if x >= n then Some () else None)
-    >> Option.isSome
+    >> Seq.map (fun x -> x >= n)
+    >> Seq.exists id
 let has3Vowels = hasAtLeast 3 isVowel
-
-let isDoubleLetter (x, y) = x = y
 
 let invalidPairs = set ["ab"; "cd"; "pq"; "xy"]
 let isInvalidPair (x, y) = Set.contains (new string([| x; y |])) invalidPairs 
 
 let matchesPairPredicate predicate = Seq.pairwise >> Seq.exists predicate
+let hasDoubleLetter = matchesPairPredicate (function | (x, y) -> x = y)
+let hasInvalidPair = not << matchesPairPredicate isInvalidPair
 
-let isNice text =
-    has3Vowels text &&
-    matchesPairPredicate isDoubleLetter text &&
-    not <| matchesPairPredicate isInvalidPair text
+let andAll functions item =
+    Seq.initInfinite (fun _ -> item)
+    |> Seq.zip functions
+    |> Seq.map (function | (f, x) -> f x)
+    |> Seq.exists not
+    |> not
+
+let isNice = andAll [has3Vowels; hasDoubleLetter; hasInvalidPair]
 
 let inputLines = System.IO.File.ReadAllLines "Day05Input.txt"
 
+let getTrueCount = Seq.sumBy (function | true -> 1 | false -> 0)
 let niceTotal = inputLines |> Seq.map isNice |> getTrueCount
