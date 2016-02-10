693e1fe8456d5ff492dc666ac580719a413707f5
Scott Fleischman <scott.fleischman@logos.com>
2016-02-10 12:52:23 -0800

    Add stub day 8.

---
+++ AdventOfCode/Day08.fs
@@ -0,0 +1,6 @@
+﻿module Day08
+
+
+
+let lines = System.IO.File.ReadAllLines "Day08Input.txt"
+let result = Seq.length lines
--- AdventOfCode/Main.fs
+++ AdventOfCode/Main.fs
@@ -7,4 +7,5 @@ module Main
 let main argv = 
     printfn "Day 5: %A" (Day05.niceTotal)
     printfn "Day 5 Part 2: %A" (Day05Part2.niceTotal)
+    printfn "Day 8: %A" (Day08.result)
     0 // return an integer exit code
