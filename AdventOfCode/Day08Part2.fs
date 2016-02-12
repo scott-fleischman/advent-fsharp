1b1e49a1d123957f83eec0d5e048b49497e2abef
Scott Fleischman <scott.fleischman@logos.com>
2016-02-11 22:37:02 -0800

    Add day 08 part 2.

---
+++ AdventOfCode/Day08Part2.fs
@@ -0,0 +1,15 @@
+﻿module Day08Part2
+
+let expand =
+    function
+    | '\\' -> ['\\'; '\\']
+    | '\"' -> ['\\'; '\"']
+    | x -> [x]
+let lines = System.IO.File.ReadAllLines "Day08Input.txt"
+let answer =
+    let newSize x =
+        let encodedLength = x |> Seq.collect expand |> Seq.length
+        2 + encodedLength
+    lines
+    |> Seq.map (fun x -> newSize x - Seq.length x)
+    |> Seq.sum
--- AdventOfCode/Main.fs
+++ AdventOfCode/Main.fs
@@ -8,4 +8,5 @@ let main argv =
     printfn "Day 5: %A" (Day05.niceTotal)
     printfn "Day 5 Part 2: %A" (Day05Part2.niceTotal)
     printfn "Day 8: %A" (Day08.answer)
+    printfn "Day 8 Part 2: %A" (Day08Part2.answer)
     0 // return an integer exit code
