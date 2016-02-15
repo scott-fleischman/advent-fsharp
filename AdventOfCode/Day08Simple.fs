750a7a6e7c5c9b9fc7b3a449080b0c98e111573c
Scott Fleischman <scott.fleischman@logos.com>
2016-02-15 02:03:21 -0800

    Tweak style.

--- AdventOfCode/Day08Simple.fs
+++ AdventOfCode/Day08Simple.fs
@@ -1,12 +1,9 @@
 ﻿module Day08Simple
 
-let lines = System.IO.File.ReadAllLines "Day08Input.txt"
+let count =
+    let hexDigits = set <| ['a'..'f'] @ ['A'..'F'] @ ['0'..'9']
+    let isHexDigit x = Set.contains x hexDigits
 
-let hexDigits = set <| ['a'..'f'] @ ['A'..'F'] @ ['0'..'9']
-let isHexDigit x = Set.contains x hexDigits
-
-let count s =
-    let s' = s |> Seq.skip 1 |> Seq.toList
     let rec go n =
         function
         | ['\"'] | [] -> n
@@ -14,8 +11,8 @@ let count s =
         | '\\' :: '\"' :: xs -> go (n + 1) xs
         | '\\' :: 'x' :: x :: y :: xs when isHexDigit x && isHexDigit y -> go (n + 1) xs
         | x :: xs -> go (n + 1) xs
-    go 0 s'
+    Seq.skip 1 >> Seq.toList >> go 0
 
 let answer =
-    lines
+    System.IO.File.ReadAllLines "Day08Input.txt"
     |> Seq.sumBy (fun x -> Seq.length x - count x)
